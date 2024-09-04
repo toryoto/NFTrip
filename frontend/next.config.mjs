217c0732dac130ed0d3a84b032fc73e056aa738b
toryoto <s1f102002345@iniad.org>
2024-09-05 02:36:51 +0900

    Pinata画像を表示させるための設定

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'chocolate-secret-cat-833.mypinata.cloud',
+        pathname: '/ipfs/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+        pathname: '/ipfs/**',
+      },
+    ]
+  },
+};
 
 export default nextConfig;
