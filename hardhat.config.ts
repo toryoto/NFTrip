492ba1c4e4c543fc05d29cbf9b700b1eb7fbceec
toryoto <s1f102002345@iniad.org>
2024-09-12 01:19:24 +0900

    sepoliaネットワークの設定

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL ?? "",
+      accounts: [process.env.PRIVATE_SEPOLIA_ACCOUNT_KEY ?? ""],
+    }
+  }
 };
 
 export default config;
