f008ea2c154adc51b75de26c36ff33c79a7bf3fb
toryoto <s1f102002345@iniad.org>
2024-09-12 01:21:43 +0900

    スマートコントラクトデプロイコードのデバッグ

--- scripts/deploy-touristNFT.ts
+++ scripts/deploy-touristNFT.ts
@@ -1,10 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const TouristNFT = await ethers.getContractFactory("TouristNFT");
-  const touristNFT = await TouristNFT.deploy();
-  await touristNFT.waitForDeployment();
-  console.log(`touristNFT deployed to: ${touristNFT.target}`);
+  console.log("Deploying TouristNFT contract...");
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contract with account:", deployer.address);
+
+  const accountBalance = await deployer.provider.getBalance(deployer.address);
+  console.log("Account balance: ", accountBalance.toString())
+
+  const TouristNFTContractFactory = await ethers.getContractFactory("TouristNFT");
+  const touristNFTContract = await TouristNFTContractFactory.deploy();
+  const touristNFT = await touristNFTContract.waitForDeployment();
+  console.log("TouristNFT deployed to:", await touristNFT.getAddress());
+  console.log("Deployment completed successfully!");
 
   console.log("Initializing locations...");
   await touristNFT.initializeLocations();
