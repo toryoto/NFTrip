009104d6be3b527058afb6b62b1af898b9433be0
toryoto <s1f102002345@iniad.org>
2024-09-12 11:46:23 +0900

    pinata処理try catchでエラー検知

--- frontend/src/lib/pinata.ts
+++ frontend/src/lib/pinata.ts
@@ -4,7 +4,12 @@ import axios from 'axios';
 export async function generateAndUploadNFTMetaData(
   imageHash: string,
   location: LocationWithThumbnailAndDistance
-) {
-  const response = await axios.post('/api/v1/pinata/nft-metadata', {imageHash, location});
-  return response.data.ipfsHash
+): Promise<string> {
+  try {
+    const response = await axios.post('/api/v1/pinata/nft-metadata', {imageHash, location});
+    return response.data.ipfsHash;
+  } catch (error) {
+    console.error('Error in generateAndUploadNFTMetaData:', error);
+    throw error;
+  }
 }
