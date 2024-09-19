12db1a79c06c2c496e4594c945199767c136615f
toryoto <ryoto.taga12@gmail.com>
2024-09-19 16:50:24 +0900

    user is possibly nullの解消

--- frontend/src/app/types/auth.ts
+++ frontend/src/app/types/auth.ts
@@ -16,7 +16,7 @@ export interface UserProfile {
 }
 
 export interface AuthContextType {
-  user: User;
+  user: User | null;
   login: (method: AuthMethod) => Promise<void>;
   logout: () => Promise<void>;
   getProvider: (method: AuthMethod) => Promise<BrowserProvider>
