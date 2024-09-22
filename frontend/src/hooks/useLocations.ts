e7142d835e36a95ef6cd22212ed9316be4a4fda0
toryoto <ryoto.taga12@gmail.com>
2024-09-22 19:47:12 +0900

    useLocationsで全ての観光地を取得する処理を行わない

--- frontend/src/hooks/useLocations.ts
+++ frontend/src/hooks/useLocations.ts
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
-import { getLocations, getNearestLocations } from "@/lib/getLocations";
-import { Location, LocationWithThumbnailAndDistance } from "@/app/types/location";
+import { getNearestLocations } from "@/lib/getLocations";
+import { LocationWithThumbnailAndDistance } from "@/app/types/location";
 
 export const useLocations = (nearestCount: number = 3) => {
   const [userLocation, setUserLocation] = useState<{ lat: number | null; lon: number | null }>({ lat: null, lon: null })
-  const [locations, setLocations] = useState<(Location & { thumbnail: string | null })[]>([]);
   const [nearestLocations, setNearestLocations] = useState<LocationWithThumbnailAndDistance[]>([]);
-  const [distances, setDistances] = useState<{ [key: string]: number }>({});
   const [loading, setLoading] = useState(true);
 
   // ユーザの位置情報を監視
@@ -56,23 +54,6 @@ export const useLocations = (nearestCount: number = 3) => {
     }
   }, []);
 
-  // 全ての観光地を取得
-  useEffect(() => {
-    const fetchLocations = async () => {
-      setLoading(true);
-      try {
-        const fetchedLocations = await getLocations();
-        setLocations(fetchedLocations);
-      } catch (error) {
-        console.error('Failed to fetch locations:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchLocations();
-  }, []);
-
   // 最寄りの場所を取得
   useEffect(() => {
     const fetchNearestLocations = async () => {
@@ -91,30 +72,5 @@ export const useLocations = (nearestCount: number = 3) => {
     fetchNearestLocations();
   }, [userLocation.lat, userLocation.lon, nearestCount]);
 
-  useEffect(() => {
-    if (userLocation.lat !== null && userLocation.lon !== null) {
-      const newDistances: { [key: number]: number } = {}
-      locations.forEach(location => {
-        const distance = calculateDistance(userLocation.lat!, userLocation.lon!, location.latitude, location.longitude)
-        newDistances[location.id] = distance
-      })
-      setDistances(newDistances)
-    }
-  }, [userLocation, locations])
-
-
-  const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-    const R = 6371 // Earth's radius in km
-    const dLat = (lat2 - lat1) * Math.PI / 180
-    const dLon = (lon2 - lon1) * Math.PI / 180
-    const a = 
-      Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
-      Math.sin(dLon/2) * Math.sin(dLon/2)
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    const distance = R * c
-    return Number(distance.toFixed(1))
-  }
-
-  return { userLocation, locations, nearestLocations, loading, distances };
+  return { userLocation, nearestLocations, loading };
 }
