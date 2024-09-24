ded3b8c118572d4e6155d3085c56f629d6e6c3a3
toryoto <ryoto.taga12@gmail.com>
2024-09-24 16:00:29 +0900

    NFT発行ようボタンをLocationDistanceコンポーネントに含める

--- frontend/src/app/components/LocationDistance.tsx
+++ frontend/src/app/components/LocationDistance.tsx
@@ -3,30 +3,36 @@
 import { MapPin } from 'lucide-react'
 import React from 'react'
 import { useLocations } from '@/hooks/useLocations'
+import { Button } from '@/components/ui/button'
 
-export const LocationDistance: React.FC<{ lat: number, lon: number }> = ({ lat, lon }) =>{
-	const { userLocation } = useLocations()
+export const LocationDistance: React.FC<{ lat: number; lon: number }> = ({ lat, lon }) => {
+  const { userLocation } = useLocations()
 
-	const calculateDistance = (lat1: number | null, lon1: number | null, lat2: number, lon2: number): number => {
-		if (lat1 === null || lon1 === null) {
-			return 0
-		}
-		const R = 6371 // Earth's radius in km
-		const dLat = (lat2 - lat1) * Math.PI / 180
-		const dLon = (lon2 - lon1) * Math.PI / 180
-		const a = 	
-			Math.sin(dLat/2) * Math.sin(dLat/2) +
-			Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
-			Math.sin(dLon/2) * Math.sin(dLon/2)
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-		const distance = R * c
-		return Number(distance.toFixed(1))
-	}
+  const calculateDistance = (lat1: number | null, lon1: number | null, lat2: number, lon2: number): number => {
+    if (lat1 === null || lon1 === null) {
+      return 0
+    }
+    const R = 6371 // Earth's radius in km
+    const dLat = (lat2 - lat1) * Math.PI / 180
+    const dLon = (lon2 - lon1) * Math.PI / 180
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    const distance = R * c
+    return Number(distance.toFixed(1))
+  }
 
   return (
-		<span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-700 text-green-400">
-			<MapPin className="h-4 w-4 mr-1 text-green-400" />
-			{calculateDistance(userLocation.lat, userLocation.lon, lat, lon)} km
-		</span>
+    <>
+      <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-700 text-green-400">
+        <MapPin className="h-4 w-4 mr-1" />
+        {calculateDistance(userLocation.lat, userLocation.lon, lat, lon)} km
+      </span>
+      <Button size="sm" className="bg-blue-600 hover:bg-blue-700 text-xs text-white">
+        GET NFT!
+      </Button>
+    </>
   )
 }
