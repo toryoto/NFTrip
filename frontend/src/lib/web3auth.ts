a97197ea354cb21e46075928b260709121826463
toryoto <ryoto.taga12@gmail.com>
2024-09-24 20:05:06 +0900

    デバッグを有効か

--- frontend/src/lib/web3auth.ts
+++ frontend/src/lib/web3auth.ts
@@ -5,13 +5,14 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 const chainConfig = {
   chainNamespace: CHAIN_NAMESPACES.EIP155,
   chainId: "0xaa36a7",
-  rpcTarget: "https://shape-sepolia.g.alchemy.com/v2/0f3pgdhbIActNECtmQus8qUy6Gl6HbwT",
+  rpcTarget: "https://eth-sepolia.g.alchemy.com/v2/0f3pgdhbIActNECtmQus8qUy6Gl6HbwT",
   displayName: "Ethereum Sepolia Testnet",
   blockExplorerUrl: "https://sepolia.etherscan.io",
   ticker: "ETH",
   tickerName: "Ethereum",
   logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
   useCoreKitKey: true,
+  enableLogging: true
 };
 const privateKeyProvider = new EthereumPrivateKeyProvider({
   config: { chainConfig },
