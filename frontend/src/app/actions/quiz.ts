985a80cdf0cc43ca392942a30a2e83b74eb265ba
toryoto <ryoto.taga12@gmail.com>
2024-10-04 15:17:43 +0900

    SupabaseからクイズをQuiz型で取得する

--- frontend/src/app/actions/quiz.ts
+++ frontend/src/app/actions/quiz.ts
@@ -1,9 +1,10 @@
 'use server'
 
 import { supabase } from "@/lib/supabase";
+import { Quiz } from "../types/quiz";
 
 // Supabaseから観光地のクイズを3つ取得するメソッド
-export async function getLocationQuizzes(locationId: number) {
+export async function getLocationQuizzes(locationId: number): Promise<Quiz[]> {
 	console.log('locationId: ', locationId)
 	// Quizzesテーブルから問題を3問、Quizzes_Optionsからその問題の選択肢を取得する
 	const { data: quizzes, error } = await supabase
@@ -20,5 +21,12 @@ export async function getLocationQuizzes(locationId: number) {
 
 	if (error) throw error;
 
-	return quizzes
+	return quizzes.map(quiz => ({
+		id: quiz.id,
+		question_text: quiz.question_text,
+		options: quiz.quizzes_options.map(option => ({
+			id: option.id,
+			option_text: option.option_text
+		}))
+	}));
 }
