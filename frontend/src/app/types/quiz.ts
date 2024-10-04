0a477d39b80bcae5e53fc08d4a577630e078a840
toryoto <ryoto.taga12@gmail.com>
2024-10-04 15:13:43 +0900

    クイズの問題と答えを分けた型定義にする

--- frontend/src/app/types/quiz.ts
+++ frontend/src/app/types/quiz.ts
@@ -2,16 +2,15 @@ export type Quiz = {
   id: number
   question_text: string
   options: QuizOption[]
-  explanation: QuizExplanation
 }
 
 export type QuizOption = {
   id: number
   option_text: string
-  is_correct: boolean
 }
 
-export type QuizExplanation = {
-  explanation_text: string
+export type QuizAnswers = {
+	correct_option_id: number
+	explanation_text: string
   additional_resources: string
 }
