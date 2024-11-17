ab36897d7a53fc27cd994f77a4e0447cbbb6130a
toryoto <ryoto.taga12@gmail.com>
2024-11-17 16:06:16 +0900

    ERC2771を使用してメタトランザクションを実行する

--- contracts/SepoliaFaucet.sol
+++ contracts/SepoliaFaucet.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SepoliaFaucet {
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import "@openzeppelin/contracts/metatx/MinimalForwarder.sol";
+
+contract SepoliaFaucet is ERC2771Context {
 	address public owner;
-	uint256 public withdrawalAmount = 0.05 ether;
+	uint256 public withdrawalAmount = 0.01 ether;
 	uint256 public lockTime = 2 days;
 
 	mapping(address => uint256) lastWithdrawTime;
@@ -16,29 +19,35 @@ contract SepoliaFaucet {
 	event WithdrawalAmountChanged(uint256 oldAmount, uint256 newAmount);
 	event LockTimeChanged(uint256 oldTime, uint256 newTime);
 
-	constructor() {
-    owner = msg.sender;
-  }
+	constructor(MinimalForwarder forwarder) ERC2771Context(address(forwarder)) {
+		// メタトランザクションをサポートするために、_msgSender()を使用して元の送信者を取得
+		owner = _msgSender();
+	}
 
 	modifier onlyOwner() {
-		require(msg.sender == owner, "Only the owner can call this function");
+		require( _msgSender() == owner, "Only the owner can call this function");
 		_;
 	}
 
 	// コントラクトがEtherを受け取った時に自動的に呼び出されるメソッド
 	receive() external payable {
-		emit Deposit(msg.sender, msg.value);
+		emit Deposit(_msgSender(), msg.value);
 	}
 
+	// 実行するのはForwarderだが結果を受け取るのは元の送信者
 	function requestTokens() external {
-		require(block.timestamp >= lastWithdrawTime[msg.sender] + lockTime, "Must wait 2 days between withdrawals");
-		require(address(this).balance >= withdrawalAmount, "Faucet is empty");
+		// メタトランザクションをサポートするために、_msgSender()を使用して元の送信者を取得
+    address sender = _msgSender();
+    require(block.timestamp >= lastWithdrawTime[sender] + lockTime, "Must wait 2 days between withdrawals");
+    require(address(this).balance >= withdrawalAmount, "Faucet is empty");
+		lastWithdrawTime[sender] = block.timestamp;
+      
+		// メタトランザクションによって元の送信者に送金
+		(bool success, ) = payable(sender).call{value: withdrawalAmount}("");
+		require(success, "Transfer failed");
 
-		lastWithdrawTime[msg.sender] = block.timestamp;
-		payable(msg.sender).transfer(withdrawalAmount);
-
-		emit Withdrawal(msg.sender, withdrawalAmount);
-	}
+		emit Withdrawal(sender, withdrawalAmount);
+  }
 
 	// ownerがトークン数を変更する
 	function setWithdrawalAmount(uint256 amount) external onlyOwner() {
