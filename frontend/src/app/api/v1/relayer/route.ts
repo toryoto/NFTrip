94c115823365e38e30ad9a4fb35fdbbdbc423e52
toryoto <ryoto.taga12@gmail.com>
2024-11-18 20:32:09 +0900

    無駄なレスポンスを削除

--- frontend/src/app/api/v1/relayer/route.ts
+++ frontend/src/app/api/v1/relayer/route.ts
@@ -1,20 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 const { Defender } = require('@openzeppelin/defender-sdk');
 import { ethers } from 'ethers';
-import SepoliaFaucet from '../../../../../abi/SepoliaFaucet.json';
 import MinimalForwarder from '../../../../../abi/MinimalForwarder.json';
 
 const FORWARDER_ADDRESS = '0x56823B1E1eFcb375774AE955cCE6B1960F083C70';
-
-async function decodeRevertReason(trace: string): Promise<string> {
-  if (trace.startsWith('0x08c379a0')) {
-    // データ部分を切り出し
-    const data = '0x' + trace.slice(10);
-    const [error] = ethers.utils.defaultAbiCoder.decode(['string'], data);
-    return error;
-  }
-  return 'Unknown error';
-}
 
 export async function POST(request: NextRequest) {
   try {
@@ -53,40 +42,9 @@ export async function POST(request: NextRequest) {
 
     const receipt = await tx.wait();
 
-		if (receipt.status === 1) {
-			const trace = await provider.call(
-				{
-					to: forwardRequest.to,
-					from: forwardRequest.from,
-					data: forwardRequest.data,
-					value: forwardRequest.value,
-				},
-				receipt.blockNumber
-			);
-			if (trace.startsWith('0x08c379a0')) {
-				const errorMessage = await decodeRevertReason(trace);
-				throw new Error(errorMessage);
-			}
-		}
-
-    const faucetInterface = new ethers.utils.Interface(SepoliaFaucet.abi);
-    const events = receipt.logs
-      .map((log: ethers.providers.Log) => {
-        try {
-          return faucetInterface.parseLog(log);
-        } catch (e) {
-          return null;
-        }
-      })
-			.filter((event: ethers.utils.LogDescription | null) => event !== null);
-
     return NextResponse.json({
       success: true,
-      receipt: {
-        ...receipt,
-        events,
-        originalSender: forwardRequest.from
-      },
+      receipt: receipt,
       metaTxInfo: {
         relayer: receipt.from,
         forwarder: receipt.to,
